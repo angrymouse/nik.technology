ad6bfd2595e348f7d9bbc5fc74d6ecc80a8a9eb5
Nik Rykov <rykov.nikas@gmail.com>
2024-09-29 03:18:09 +0200

    webring

--- app.vue
+++ app.vue
@@ -5,6 +5,13 @@
     <NuxtPage />
 
     <footer class="bg-black p-4 mt-12 border-t-2 border-neon-green">
+      <div class="container mx-auto text-center flex flex-col justify-center items-center">
+        <div>Webring:</div>
+        <div class="flex flex-row justify-center">
+          <a href="https://apiv2.li0ard.rest/webring/prev?url=nik.technology" class="text-neon-green underline px-2 w-16">Previous</a>
+          <a href="https://apiv2.li0ard.rest/webring/next?url=nik.technology" class="text-neon-green underline px-2 w-16">Next</a>
+        </div>
+      </div>
       <div class="container mx-auto text-center">
         <p>&copy; 2024 Nik Rykov. All rights reserved.</p>
       </div>
